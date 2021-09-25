d6aca26eee1690011efcac722bbe8611245dc9c7
fedinskiy <>
2021-09-25 14:37:14 +0200

    ~Formatting

--- README.md
+++ README.md
@@ -5,11 +5,12 @@ Web application, counts symbols in a text, including and excluding spaces. Prope
 
 Same as https://github.com/fedinskiy/symbol-counter, but rewritten in Rust with warp and askama.
 
-##Build:
+## Build:
 `cargo build`
 
 
-##Cross-build:
+## Cross-build:
 1. Install a tool for a crosslinking and cross-compilation(https://github.com/rust-embedded/cross)
 `cargo install cross`
+2. Build an executable file
 `cross build --target armv7-unknown-linux-gnueabihf`
--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,15 @@
 use std::collections::HashMap;
 use std::convert::Infallible;
 
-use warp::{http::Uri, Filter, Rejection, Reply};
 use warp::http::StatusCode;
+use warp::{http::Uri, Filter, Rejection, Reply};
 
 use askama::Template;
 
-
 #[tokio::main]
 async fn main() {
-	let root = warp::path::end().map(|| {
-		warp::redirect(Uri::from_static("counter"))
-	});
-	let counter=warp::path!("counter");
+	let root = warp::path::end().map(|| warp::redirect(Uri::from_static("counter")));
+	let counter = warp::path!("counter");
 
 	let input_page = counter
 		.and(warp::get())
@@ -21,7 +18,7 @@ async fn main() {
 			data.render().unwrap()
 		})
 		.map(|rendered| warp::reply::html(rendered));
-		
+
 	let results = counter
 		.and(warp::post())
 		.and(warp::body::content_length_limit(1024 * 100))
@@ -33,34 +30,44 @@ async fn main() {
 		.map(|rendered| warp::reply::html(rendered));
 
 	let routes = root.or(input_page).or(results).recover(bad_request);
-	
+
 	println!("Starting the application");
-	warp::serve(routes)
-		.run(([0,0,0,0],3030))
-	.await;
+	warp::serve(routes).run(([0, 0, 0, 0], 3030)).await;
 }
 
 fn get_input() -> impl Filter<Extract = (String,), Error = Rejection> + Copy {
-    warp::body::form().and_then(|mut content: HashMap<String, String> | async move {
-        if let Some(input) = content.remove("input") {
-            Ok(input)
-        } else {
-            Err(warp::reject::custom(EmptyBody))
-        }
-    })
+	warp::body::form().and_then(|mut content: HashMap<String, String>| async move {
+		if let Some(input) = content.remove("input") {
+			Ok(input)
+		} else {
+			Err(warp::reject::custom(EmptyBody))
+		}
+	})
 }
 
 async fn bad_request(err: Rejection) -> Result<impl Reply, Infallible> {
-	println!("{:?}",err);
+	println!("{:?}", err);
 	if err.is_not_found() {
-		 Ok(warp::reply::with_status("Неверный путь", StatusCode::NOT_FOUND))
+		Ok(warp::reply::with_status(
+			"Неверный путь",
+			StatusCode::NOT_FOUND,
+		))
 	} else if let Some(_) = err.find::<EmptyBody>() {
-		Ok(warp::reply::with_status("Отсутствует поле!", StatusCode::BAD_REQUEST))
+		Ok(warp::reply::with_status(
+			"Отсутствует поле!",
+			StatusCode::BAD_REQUEST,
+		))
 	} else if let Some(_) = err.find::<warp::reject::PayloadTooLarge>() {
-		Ok(warp::reply::with_status("Слишком длинный текст!", StatusCode::BAD_REQUEST))
+		Ok(warp::reply::with_status(
+			"Слишком длинный текст!",
+			StatusCode::BAD_REQUEST,
+		))
 	} else {
-		Ok(warp::reply::with_status("Bad Request", StatusCode::BAD_REQUEST))
-	 }
+		Ok(warp::reply::with_status(
+			"Bad Request",
+			StatusCode::BAD_REQUEST,
+		))
+	}
 }
 
 #[derive(Debug)]
@@ -68,7 +75,7 @@ struct EmptyBody;
 impl warp::reject::Reject for EmptyBody {}
 
 #[derive(Template)]
-#[template(path="test.html")]
+#[template(path = "test.html")]
 struct Page {
 	was: String,
 	letters: usize,
@@ -76,23 +83,22 @@ struct Page {
 }
 
 impl Page {
-	fn create(s:String)->Page {
-		let mut total:usize=0;
-		let mut letters:usize=0;
-		s.chars()
-			.for_each(|letter|{
-				if !(letter=='\r'){
-					total+=1;
-				}
-				if !letter.is_whitespace() {
-					letters+=1;
-				}
-			});
-		
+	fn create(s: String) -> Page {
+		let mut total: usize = 0;
+		let mut letters: usize = 0;
+		s.chars().for_each(|letter| {
+			if !(letter == '\r') {
+				total += 1;
+			}
+			if !letter.is_whitespace() {
+				letters += 1;
+			}
+		});
+
 		Page {
-			letters: letters, 
+			letters: letters,
 			length: total,
-			was: s
+			was: s,
 		}
 	}
 	fn init() -> Page {
